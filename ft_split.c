f163c78a521eda3dee2aaa8fa0805d8a8e048a4b
MiguelMeireles09 <miguelmeireles025@gmail.com>
2024-10-26 00:52:40 +0100

    added ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:26:15 by jmeirele          #+#    #+#             */
-/*   Updated: 2024/10/25 17:43:28 by jmeirele         ###   ########.fr       */
+/*   Updated: 2024/10/26 00:51:58 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,60 +18,94 @@
 
 static int	ft_count_words(char const *str, char c)
 {
-	size_t	i;
 	size_t	counter;
+	size_t	i;
 
-	counter = 0;
 	i = 0;
-
+	counter = 0;
 	while (str[i])
+	{
+		while (str[i] && str[i] == c)
+			i++;
+		if (str[i] && str[i] != c)
+		{
+			counter++;
+			while (str[i] && str[i] != c)
+				i++;
+		}
+	}
 	return (counter);
 }
-static int	ft_word_len(char const *str, char c)
+static char	*ft_strndup(const char *s, size_t n)
 {
-	size_t	len;
-
-	len = 0;
-	while (str[len] && str[len] != c)
-		len++;
-	return (len);
-}
-static char	*ft_word_dup(const char *str, size_t start, size_t end)
-{
-	char	*word;
 	size_t	i;
 
 	i = 0;
-	word = malloc(sizeof(char) * (end - start + 1));
-	if (!word)
+	char	*dup = malloc(n + 1);
+	if(!dup)
 		return (NULL);
-	while (start < end)
-		word[i++] = str[start++];
-	word[i] = '\0';
-	return (word);
+	while (i < n)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[n] = '\0';
+	return (dup);
 }
 
+static void	ft_fill(char **new, const char *s, char c)
+{
+	size_t	i = 0;
+	size_t	z = 0;
+	size_t	word_len;
+
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		word_len = 0;
+		while (s[i + word_len] && s[i + word_len] != c)
+			word_len++;
+		if (word_len > 0)
+		{
+			new[z++] = ft_strndup(s + i, word_len);
+			i += word_len;
+		}
+	}
+}
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	words_counter;
+	char	**new;
 
+	if (!s)
+		return (NULL);
 	words_counter = ft_count_words(s, c);
-	printf("%zu", words_counter);
-	
+	new = malloc(sizeof(char *) * (words_counter + 1));
+	if(!new)
+		return (NULL);
+	new[words_counter] = NULL;
+	ft_fill(new, s, c);
+	return (new);
 }
 
-int	main(void)
+/* int	main(void)
 {
-	char	*str = "eeHeeello World , Goodbye Fellas !e";
-	char	c = 'e';
+	char	*str = "Heeello World , Goodbye Fellas !e eoklaeeee";
+	char	*str1 = "      split       this for   me  !       ";
+	char	c = ' ';
 	char	**res;
 	size_t	i;
 
-	ft_split(str, c);
 	i = 0;
-	res = ft_split(str, c);
-	// for(int i; str[i]; i++)
-	// 	printf("%s", res[i]);
-	// free(res);
-}
+	res = ft_split(str1, c);
+	while (res[i])
+	{
+		printf("%s\n", res[i]);
+		free(res[i]);
+		i++;
+	}
+	free(res);
+	return (0);
+} */
