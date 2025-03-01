0d5bbac0d616378d358d9ff920d82c9bc3ad03de
Jose Meireles <jmeirele@c2r2s5.42porto.com>
2025-03-01 12:15:00 +0000

    fixed norminette

--- FT_Printf/src/ft_print_str.c
+++ FT_Printf/src/ft_print_str.c
@@ -6,11 +6,12 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:34 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/03/01 11:09:37 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/03/01 12:14:55 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
+
 int	ft_print_str(int fd, char *str)
 {
 	int	counter;
