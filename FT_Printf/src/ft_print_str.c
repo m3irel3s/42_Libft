96236fb6519255c26588a9c6623a2cb25399a34b
Jose Meireles <jmeirele@c2r2s5.42porto.com>
2025-03-01 11:07:54 +0000

    fix ft_printf

--- FT_Printf/src/ft_print_str.c
+++ FT_Printf/src/ft_print_str.c
@@ -6,12 +6,12 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:34 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/27 17:17:09 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:07:47 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
-int	ft_print_str(int fd, char *str)
+/* int	ft_print_str(int fd, char *str)
 {
 	int	counter;
 
@@ -25,26 +25,26 @@ int	ft_print_str(int fd, char *str)
 		counter++;
 	}
 	return (counter);
+} */
+
+static size_t	ft_strlen_pf(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
-// 
-// static size_t	ft_strlen_pf(const char *str)
-// {
-// 	size_t	i;
 
-// 	i = 0;
-// 	while (str[i])
-// 		i++;
-// 	return (i);
-// }
+int	ft_print_str(int fd, char *str)
+{
+	int	counter;
 
-// int	ft_print_str(int fd, char *str)
-// {
-// 	int	counter;
-
-// 	if (!str)
-// 		return (ft_print_str(fd, "(null)"));
-// 	counter = 0;
-// 	write(fd, str, ft_strlen_pf(str));
-// 	counter = ft_strlen_pf(str);
-// 	return (counter);
-// }
+	if (!str)
+		return (ft_print_str(fd, "(null)"));
+	counter = 0;
+	write(fd, str, ft_strlen_pf(str));
+	counter = ft_strlen_pf(str);
+	return (counter);
+}
