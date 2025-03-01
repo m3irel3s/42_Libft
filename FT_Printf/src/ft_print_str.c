6e2d985f40960e99464983b6c28a0fbc1d42abac
Jose Meireles <jmeirele@c2r2s5.42porto.com>
2025-03-01 11:09:52 +0000

    fix printf

--- FT_Printf/src/ft_print_str.c
+++ FT_Printf/src/ft_print_str.c
@@ -6,12 +6,12 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:34 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/03/01 11:07:47 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:09:37 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
-/* int	ft_print_str(int fd, char *str)
+int	ft_print_str(int fd, char *str)
 {
 	int	counter;
 
@@ -24,27 +24,5 @@
 		str++;
 		counter++;
 	}
-	return (counter);
-} */
-
-static size_t	ft_strlen_pf(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	ft_print_str(int fd, char *str)
-{
-	int	counter;
-
-	if (!str)
-		return (ft_print_str(fd, "(null)"));
-	counter = 0;
-	write(fd, str, ft_strlen_pf(str));
-	counter = ft_strlen_pf(str);
 	return (counter);
 }
